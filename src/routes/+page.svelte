7131f9aa32e0b5d7112fc639dd13cf36ce30cbfa
conrmahr <conor@hireconor.com>
2025-09-19 11:49:52 -0500

    fix: broken image when avatar isn't set

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -261,7 +261,7 @@
 									<td class="whitespace-nowrap py-5 pl-4 pr-3 text-sm sm:pl-0">
 										<div class="flex items-center">
 											<div class="h-11 w-11 shrink-0">
-												<img class="h-11 w-11 rounded-full" src={sub.thumbnail} alt="" />
+												<img class="h-11 w-11 rounded-full" src={sub.thumbnail?.startsWith('http') ? sub.thumbnail : '/feed-icon.svg'} alt="" />
 											</div>
 											<div class="ml-4">
 												<div class="font-medium text-gray-900">{sub.name}</div>
